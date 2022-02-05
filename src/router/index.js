4daf9cdcad9611fe216e5287b18970030201d584
Emaleff <emalev91@gmail.com>
2022-02-06 00:06:15 +0700

    second

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import InfoCountry from "../components/InfoCountry";
 
 export default new VueRouter({
   routes: [
-    { path: "", name: "main", component: Content },
+    { path: "/", name: "main", component: Content },
     { path: "/InfoCountry", component: InfoCountry, name: "info", props: true },
   ],
   mode: "history",
